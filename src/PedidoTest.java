63356b82ae5eb2624a280d6ab920b4cad6d3de70
Alexandre <lexandre.guimaraes@gmail.com>
2026-09-14 12:03:05 -0300

    Completado test pedido

--- src/PedidoTest.java
+++ src/PedidoTest.java
@@ -1,14 +1,18 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PedidoTest {
-    
+    Pedido pedido;
+    Pizza pizzaVazia;
+
+
     @Test
     public void naoAdicionaPizzaEmPedidoFechado(){
-        //Arrange
-        Pedido pedido = new Pedido();
-        pedido.adicionarPizza(new Pizza());
+        //Arranges
         pedido.fecharPedido();
 
         //Act
@@ -16,5 +20,53 @@ public class PedidoTest {
     
         //Assert
         assertEquals(1, quantidade);
+    }
+
+    @Test
+    public void adicionaVariasPizzaCorretamente(){
+        //Art
+        pedido.adicionarPizza(new Pizza());
+        pedido.adicionarPizza(new Pizza());
+        pedido.adicionarPizza(new Pizza());
+        int quantidade = pedido.adicionarPizza(new Pizza());
+        //Assert
+        assertEquals(5, quantidade);
+    }
+
+    @BeforeEach
+    public void setUp(){
+        pedido = new Pedido();
+        pizzaVazia = new Pizza();
+        pedido.adicionarPizza(pizzaVazia);
+    }
+
+    @Test
+    public void calculaValorPedidoCorretamente(){
+        //Act
+        double preco =  pedido.precoAPagar();
+        //Asset
+        assertEquals(29d, preco,0.01);
+    }
+
+    @Test
+    public void calcularPrecoDePedidoComVariasPizzas(){
+        //Arrange
+        Pizza pizza2Ingrediente = new Pizza(2);
+        pedido.adicionarPizza(pizza2Ingrediente);
+        //Act
+        double preco = pedido.precoAPagar();
+        //Assert
+        assertEquals(68d, preco,0.01);
+    }
+
+    @Test
+    public void gerarRelatorioDePedido(){
+        //Act
+        String cupom = pedido.relatorio();
+        //Assert
+        assertTrue(cupom.contains("29,00")&&
+            cupom.contains("1 pizza")&&
+            cupom.contains("aberto")
+        );
     }
 }
